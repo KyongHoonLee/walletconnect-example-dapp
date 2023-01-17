8862b760b86c5f982d1021a84c4a0351d458016e
Kyonghoon Lee <kyonghoon.lee@sk.com>
2023-01-17 23:28:38 +0900

    Updates

---
+++ precache-manifest.873e7b833b54418f942229f16925e53f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "eac7217b77f6a5f56e3a",
+    "url": "/walletconnect-example-dapp/static/js/main.eac7217b.chunk.js"
+  },
+  {
+    "revision": "540304f6690119738e68",
+    "url": "/walletconnect-example-dapp/static/js/1.540304f6.chunk.js"
+  },
+  {
+    "revision": "b9fb507d40c767f08e4e",
+    "url": "/walletconnect-example-dapp/static/js/runtime~main.b9fb507d.js"
+  },
+  {
+    "revision": "e1cb8d21bffbcc2a8c7bfcf95bad12ff",
+    "url": "/walletconnect-example-dapp/static/media/walletconnect-banner.e1cb8d21.png"
+  },
+  {
+    "revision": "906078994f7221a25b900f47425b3ecc",
+    "url": "/walletconnect-example-dapp/static/media/erc20.90607899.svg"
+  },
+  {
+    "revision": "42bca7317690c3f5e9ecd8db387344df",
+    "url": "/walletconnect-example-dapp/static/media/eth.42bca731.svg"
+  },
+  {
+    "revision": "e37299a879ceb5a23000d8a2bb10bc46",
+    "url": "/walletconnect-example-dapp/static/media/xdai.e37299a8.png"
+  },
+  {
+    "revision": "585a0a758f464c2b24ab356a52718bd7",
+    "url": "/walletconnect-example-dapp/index.html"
+  }
+];
